2fd805d9ff3f2da6ee7f28b664d4dfc667675960
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2024-12-07 06:26:41 +0300

    Tweak deploy method

--- server.sh
+++ server.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-export NITRO_SSL_CERT="`cat /etc/ssl/certs/financial-compass-app_ru.crt`"
-export NITRO_SSL_KEY="`cat /etc/ssl/private/financial-compass-app_ru.key`"
-export PORT=443
-export NODE_ENV=production
+set -a
+source .env.production
 
 pm2 stop financial-compass
-pm2 start .output/server/index.mjs --name financial-compass
+pm2 start .output/server/index.mjs --name financial-compass --update-env
