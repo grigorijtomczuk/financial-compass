88cb03313e3b3c5182138a591aff10b7b125e77f
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2024-12-07 06:47:22 +0300

    Tweak deploy method 3

--- server.sh
+++ server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -a
-source .env.production
+source .env.production &&
 
-npm run build
+npm run build &&
 
 pm2 stop financial-compass
 pm2 start .output/server/index.mjs --name financial-compass --update-env
