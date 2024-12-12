88cb507137ee5ddb61ed00e2395cf2126c76a00f
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2024-12-12 05:42:03 +0300

    Fix things

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-import { enabled } from "virtual:nuxt-pwa-configuration";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
